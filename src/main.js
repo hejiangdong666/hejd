0b6d8504f26fd9586266d1c18a11e30b101ed238
hjd <hjd@yht.com>
2020-08-11 12:35:52 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,68 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import { getStore } from '@/utils/storage';
+
+
+// 使用vue-lazyload
+import VueLazyload from 'vue-lazyload'
+Vue.use(VueLazyload);
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'static/images/error.png',
+  loading: 'static/images/load.gif',
+  attempt: 1
+})
 
 Vue.config.productionTip = false
+// 挂载axios到vue的原型 ，由于继承性，所有的组件都可以使用this.$http
+import axios from 'axios';
+Vue.prototype.$http = axios;
+axios.interceptors.request.use(config => {
+  const token = getStore('token')
+  if (token) {
+    // 表示用户已登录
+    config.headers.common['Authorization'] = token;
+  }
+  return config
+}, error => {
+  return Promise.reject(error);
+})
+// 守卫
+router.beforeEach((to, from, next) => {
+  axios.post('api/validate', {}).then(res => {
+    let data = res.data;
+    if (data.state !== 1) {
+      // 用户要登录
+      if (to.matched.some(record => record.meta.auth)) {
+        // 用户未登录  需要跳转登录页面
+        next({
+          path: '/login',
+          query: {
+            redirect: to.fullPath
+          }
+        })
+      } else {
+        next();
+      }
+    } else {
+      // 保存用户的信息
+      store.commit('ISLOGIN', data);
+      if (to.path === '/login') {
+        router.push({
+          path: '/'
+        })
+      }
+
+    }
+  }).catch(error => {
+    console.log(error)
+  })
+  next();
+
+})
 
 new Vue({
   router,
---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+     <m-header></m-header>
+     <router-view></router-view>
+    </div>
+</template>
+
+<script>
+  import MHeader from '@/common/MHeader'
+    export default {
+        components:{
+            MHeader,
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
